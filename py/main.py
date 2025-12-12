22bcef210fcdb9b8079adfae46a68ae15174ce2b
Jesper <jesper.l.richert@gmail.com>
2025-12-12 22:29:02 +0100

    Updated py code for the final version
    Refactor code to improve readability and update font path.

--- py/main.py
+++ py/main.py
@@ -1,11 +1,13 @@
 import time
 import sys
 import os
+from pathlib import Path
+from time import gmtime, strftime
 libdir = os.path.join(os.path.dirname(os.path.dirname(os.path.realpath(__file__))), 'drive')
 if os.path.exists(libdir):
     sys.path.append(libdir)
 
-import logging    
+import logging
 from drive import SSD1305
 
 from PIL import Image,ImageDraw,ImageFont
@@ -48,34 +50,28 @@ x = 0
 
 # Alternatively load a TTF font.  Make sure the .ttf font file is in the same directory as the python script!
 # Some other nice fonts to try: http://www.dafont.com/bitmap.php
-font = ImageFont.truetype('04B_08__.TTF',8)
+font = ImageFont.truetype('/root/display/04B_08__.TTF', size=16)
+try:
+    # Draw a black filled box to clear the image.
+    draw.rectangle((0,0,width,height), outline=0, fill=0)
 
-while True:
-    try:
-        # Draw a black filled box to clear the image.
-        draw.rectangle((0,0,width,height), outline=0, fill=0)
+    # Shell scripts for system monitoring from here : https://unix.stackexchange.com/questions/119126/command-to-display-memory-usage-disk-usage-and-cpu-load
+    cmd = "hostname -I | cut -d\' \' -f1"
+    IP = subprocess.check_output(cmd, shell = True )
 
-        # Shell scripts for system monitoring from here : https://unix.stackexchange.com/questions/119126/command-to-display-memory-usage-disk-usage-and-cpu-load
-        cmd = "hostname -I | cut -d\' \' -f1"
-        IP = subprocess.check_output(cmd, shell = True )
-        cmd = "top -bn1 | grep load | awk '{printf \"CPU Load: %.2f\", $(NF-2)}'"
-        CPU = subprocess.check_output(cmd, shell = True )
-        cmd = "free -m | awk 'NR==2{printf \"Mem: %s/%sMB %.2f%%\", $3,$2,$3*100/$2 }'"
-        MemUsage = subprocess.check_output(cmd, shell = True )
-        cmd = "df -h | awk '$NF==\"/\"{printf \"Disk: %d/%dGB %s\", $3,$2,$5}'"
-        Disk = subprocess.check_output(cmd, shell = True )
+    nameTXT = Path("/home/pi/name.txt")
+    if (nameTXT.exists()):
+        draw.text((x, top),     str(nameTXT.read_text()), font=font, fill=255)
+        draw.text((x, top+16),       str(IP.decode('utf-8')),  font=font, fill=255)
+    else:
+        draw.text((x, top+16),       str(IP.decode('utf-8')),  font=font, fill=255)
 
-        # Write two lines of text.
-        draw.text((x, top),       "IP: " + str(IP.decode('utf-8')),  font=font, fill=255)
-        draw.text((x, top+8),     str(CPU.decode('utf-8')), font=font, fill=255)
-        draw.text((x, top+16),    str(MemUsage.decode('utf-8')),  font=font, fill=255)
-        draw.text((x, top+24),    str(Disk.decode('utf-8')),  font=font, fill=255)
-        # Display image.
-        disp.getbuffer(image)
-        disp.ShowImage()
-        time.sleep(.1)
-    except(KeyboardInterrupt):
-        print("\n")
-        break
+    # Write two lines of text.
 
 
+    # Display image.
+    disp.getbuffer(image)
+    disp.ShowImage()
+    time.sleep(1)
+except(KeyboardInterrupt):
+    print("\n")
