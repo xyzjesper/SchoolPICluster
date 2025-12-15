1eb820d77b09b3079023d74e5b6c856767be1973
Jesper <jesper.l.richert@gmail.com>
2025-12-15 08:21:25 +0100

    Refactor meins.sh to streamline user handling
    Simplified script logic by removing redundant checks and combining user data handling into a single flow.

--- bash/meins.sh
+++ bash/meins.sh
@@ -1,50 +1,15 @@
 #!/bin/bash
 
-#  Global Variabels
 NAME=$1
 PASSWORD=$2
-USERFILE="/home/pi/meins.txt"
+USERFILE="/home/pi/name.txt"
 
-
-# Check Command Parameter
-if [[ ! $NAME && ! $PASSWORD ]]; then
-    echo "Use command <name> <password>"
+if [[ ! $NAME || ! $PASSWORD ]]; then
+    echo "Bitte Skript wie folgt aufrufen:"
+    echo "./meinsmeinsmeins.sh <Vorname> <Passwort>"
     exit 1
 fi
 
-if [[ ! $NAME  ]]; then
-    echo "No name provided in command."
-    exit 1
-fi
-if [[ ! $PASSWORD  ]]; then
-    echo "No password provided in command."
-    exit 1
-fi
-
-
-if [ -f $USERFILE ]; then
-    echo "\"Meins Meins\" will override the current data!"
-    echo "Claiming the PaspberryPI and update the password"
-
-    echo "Override the userdata in $USERFILE"
-    echo $NAME >> $USERFILE 
-
-    echo "Changing password to your input."
-    echo "pi:$PASSWORD" | sudo chpasswd
-
-    echo "Reboot the PaspberryPI"
-    reboot
-else
-    echo "You want to claim this PaspberryPI as \"meins\"..."
-    echo "Claiming the PaspberryPI and update the password"
-    
-    echo $NAME >> $USERFILE 
-
-    echo "pi:$PASSWORD" | sudo chpasswd
-
-    echo "Reboot the PaspberryPI"
-    reboot
-fi
-
-
-
+echo $NAME > $USERFILE 
+echo "pi:$PASSWORD" | sudo chpasswd
+sudo reboot
